7a8fba8179faa240b085dcdef1e3d88222942544
Vladimir <stanlyzoolo@gmail.com>
2021-11-02 12:13:40 +0300

    django allauth connected

--- docto/urls.py
+++ docto/urls.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
-
-
 urlpatterns = [
     # Django admin
     path("admin/", admin.site.urls),
